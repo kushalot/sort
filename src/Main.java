3a82f8619242fc5423b4e172571032c4dda627d7
kushalot <38114239+kushalot@users.noreply.github.com>
2019-07-17 12:47:25 +0300

    bublesort

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введи размер массива");
+        int size = scan.nextInt();
+        int[] nums = new int[size];
+        System.out.println("Введи массив");
+        for (int i = 0;i < size;i++)
+            nums[i] = scan.nextInt();
+        for (int i = 0;i < size;i++)
+            System.out.print(nums[i]+" ");
+
+        int n = nums.length;
+            for (int i = 0;i < n-1;i++)
+                for (int j = 0;j < n-i-1;j++)
+                    if (nums[j]<nums[j+1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j+1];
+                        nums[j+1] = temp;
+                    }
+        System.out.println(" ");
+        for (int i = 0;i < size;i++)
+            System.out.print(nums[i]+" ");
+
+
+    }
+
+}
