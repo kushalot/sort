3f3e088d90891dc49586e4e933fb198479ed2159
kushalot <38114239+kushalot@users.noreply.github.com>
2019-07-17 12:54:45 +0300

    no hello world

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Scanner scan = new Scanner(System.in);
         System.out.println("Введи размер массива");
         int size = scan.nextInt();
