cd916e835daf25114f3c424ccb14ff5508461a13
kushalot <38114239+kushalot@users.noreply.github.com>
2019-08-05 13:01:27 +0300

    quick sort

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,53 @@
-import java.util.Scanner;
-
+import java.util.Arrays;
 public class Main {
-
     public static void main(String[] args) {
-        message("polnaja jopa  s noutom");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введи размер массива");
-        int size = scan.nextInt();
-        int[] nums = new int[size];
-        System.out.println("Введи массив");
-        for (int i = 0;i < size;i++)
-            nums[i] = scan.nextInt();
-        for (int i = 0;i < size;i++)
-            System.out.print(nums[i]+" ");
+        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
+        System.out.println("Было");
+        System.out.println(Arrays.toString(x));
 
-        int n = nums.length;
-            for (int i = 0;i < n-1;i++)
-                for (int j = 0;j < n-i-1;j++)
-                    if (nums[j]<nums[j+1])
-                    {
-                        int temp = nums[j];
-                        nums[j] = nums[j+1];
-                        nums[j+1] = temp;
-                    }
-        System.out.println(" ");
-        for (int i = 0;i < size;i++)
-            System.out.print(nums[i]+" ");
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(x));
     }
-    static void message (String s){
-        System.out.println(s);
-        System.out.print("xxaxaxaxa");
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;//завершить выполнение если длина массива равна 0
+
+        if (low >= high)
+            return;//завершить выполнение если уже нечего делить
+
+        // выбрать опорный элемент
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
+
+            while (array[j] > opora) {
+                j--;
+            }
+
+            if (i <= j) {//меняем местами
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // вызов рекурсии для сортировки левой и правой части
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
     }
-
-
 }
